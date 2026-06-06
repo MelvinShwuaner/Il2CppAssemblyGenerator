bbd79470e49e49d6d150de4df2330e350d84e057
Melvin "Dumbass" Shwuaner <83349478+MelvinShwuaner@users.noreply.github.com>
2026-06-06 17:18:13 +0300

    mhmm

--- Il2CppAssemblyGenerator/obj/Release/net10.0/win-x64/Il2CppAssemblyGenerator.AssemblyInfo.cs
+++ Il2CppAssemblyGenerator/obj/Release/net10.0/win-x64/Il2CppAssemblyGenerator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Il2CppAssemblyGenerator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+813e887bd6ea28b7927fdba8f82362a5ebff17c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03823c5f65becf5b511e28ffcb081172b6bec688")]
 [assembly: System.Reflection.AssemblyProductAttribute("Il2CppAssemblyGenerator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Il2CppAssemblyGenerator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
